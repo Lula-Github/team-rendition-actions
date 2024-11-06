79e4b9e7b0e32a0056ba6520e5600c3c9f30c462
Liam Beeton <liam.beeton@lulalend.co.za>
2024-11-06 14:25:45 +0200

    feat: add logger console colors

--- src/AppLogger/Logger.cs
+++ src/AppLogger/Logger.cs
@@ -18,6 +18,33 @@ public class Logger : ILogger
 
     public void Log(LogLevel level, string message)
     {
-        Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.fff}] [{level}] {message}");
+        var log = CreateLogEntry(level, message);
+        WriteToConsole(level, log);
+    }
+
+    private string CreateLogEntry(LogLevel level, string message)
+    {
+        return $"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.fff}] [{level}] {message}";
+    }
+
+    private void WriteToConsole(LogLevel level, string message)
+    {
+        ConsoleColor originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = GetColorForLogLevel(level);
+        Console.WriteLine(message);
+        Console.ForegroundColor = originalColor;
+    }
+
+    private ConsoleColor GetColorForLogLevel(LogLevel level)
+    {
+        return level switch
+        {
+            LogLevel.Debug => ConsoleColor.Gray,
+            LogLevel.Info => ConsoleColor.Green,
+            LogLevel.Warning => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.Red,
+            LogLevel.Critical => ConsoleColor.DarkRed,
+            _ => ConsoleColor.White
+        };
     }
 }

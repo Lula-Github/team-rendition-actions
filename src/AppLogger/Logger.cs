36a940bd854ddaf68f709537db02fda8740aa48f
Liam Beeton <liam.beeton@lulalend.co.za>
2024-11-06 14:13:44 +0200

    feat: add logger log levels

--- src/AppLogger/Logger.cs
+++ src/AppLogger/Logger.cs
@@ -1,9 +1,23 @@
 ﻿namespace AppLogger;
 
+public enum LogLevel
+{
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Critical
+}
+
 public class Logger : ILogger
 {
     public void Log(string text)
     {
         Console.WriteLine(text);
+    }
+
+    public void Log(LogLevel level, string message)
+    {
+        Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.fff}] [{level}] {message}");
     }
 }
